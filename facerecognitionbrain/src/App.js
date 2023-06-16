eeb2324b6f7a45476809c3056e63e6abd8314b37
dhia88 <dhaouadi.mohamed.dhia@hotmail.fr>
2023-06-16 03:07:37 -0400

    building the Navigation component

--- facerecognitionbrain/src/App.js
+++ facerecognitionbrain/src/App.js
@@ -1,11 +1,14 @@
+import React from 'react';
 import './App.css';
+import Navigation from './components/Navigation/Navigation';
 
 function App() {
   return (
     <div className="App">
-     
-     
-     
+     <Navigation/>
+     { /*<Logo/>
+     <ImageLinkForm/>
+     <FaceRecognition/>   */}   
     </div>
   );
 }
---
+++ facerecognitionbrain/src/components/Navigation/Navigation.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Navigation = ()=> {
+return(
+<nav style={{display: "flex", justifyContent:"end"}}>
+<p>Sign Out</p>
+</nav>
+)
+
+}
+
+export default Navigation;
