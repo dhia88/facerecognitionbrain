e589abee8cf085454fd68fe9c90e7f97fa9275b3
dhia88 <dhaouadi.mohamed.dhia@hotmail.fr>
2023-06-19 03:08:42 -0400

    added the components except  imagerecognition

--- facerecognitionbrain/src/App.js
+++ facerecognitionbrain/src/App.js
@@ -1,16 +1,22 @@
 import React from "react";
 import Navigation from "./components/Navigation/Navigation";
-
+import Logo from "./components/Logo/Logo";
+import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
+import Rank from "./components/Rank/Rank";
+import "./App.css";
+import Particle from "./components/Particles/Particle";
 
 function App() {
   return (
     <div className="App">
-     <Navigation/>
-     {/*
-      <Logo/>
-      <ImageLinkForm/>
+      <Particle />
+      <Navigation />
+      <Logo />
+      <Rank />
+      <ImageLinkForm />
+      {/*      
       <FaceRecognition/>
-  */}
+       */}
     </div>
   );
 }
--- facerecognitionbrain/src/components/Logo/Logo.js
+++ facerecognitionbrain/src/components/Logo/Logo.js
@@ -1,12 +1,16 @@
 import React from "react";
+import Tilt from "react-parallax-tilt";
+import "./Logo.css";
+import brain from './brain.png' ;
 
-const Logo =() => {
-    return(
-        <div className='ma4 mt0'>
-
-        </div>
-    )
-}
-
+const Logo = () => {
+  return (
+    <div className="ma4 mt0">
+      <Tilt className="Tilt br2 shadow-2" perspective={500} style={{height: 100, width:100}}>
+        <div className="Tilt-inner"><img src={brain} alt="logo"/></div>
+      </Tilt>
+    </div>
+  );
+};
 
 export default Logo;
