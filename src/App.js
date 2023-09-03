da975170458c246cc66e383d70818fe82c25de33
dhia88 <dhaouadi.mohamed.dhia@hotmail.fr>
2023-09-03 02:23:27 -0400

    initialState

--- src/App.js
+++ src/App.js
@@ -43,23 +43,25 @@ const returnClarifaiRequestOptions = (imageUrl) => {
   return requestOptions;
 };
 
+const initialState = {
+  input: "",
+  imageUrl: "",
+  box: {},
+  route: "signin",
+  isSignedIn: false,
+  user: {
+    id: "",
+    name: "",
+    email: "",
+    entries: "",
+    joined: "",
+  },
+};
+
 class App extends Component {
   constructor() {
     super();
-    this.state = {
-      input: "",
-      imageUrl: "",
-      box: {},
-      route: "signin",
-      isSignedIn: false,
-      user: {
-        id: "",
-        name: "",
-        email: "",
-        entries: "",
-        joined: "",
-      },
-    };
+    this.state = initialState;
     this.onInputChange = this.onInputChange.bind(this);
     this.onButtonSubmit = this.onButtonSubmit.bind(this);
   }
@@ -129,7 +131,7 @@ class App extends Component {
 
   onRouteChange = (route) => {
     if (route === "signout") {
-      this.setState({ isSignedIn: false });
+      this.setState(initialState);
     } else if (route === "home") {
       this.setState({ isSignedIn: true });
     }
